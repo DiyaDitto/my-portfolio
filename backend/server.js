aecb0fe5ddb6cd1465c0d5538ea3360561803d8f
DiyaDitto <diyaditto84@gmail.com>
2026-06-11 14:37:36 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-
 
 
 app.use(
